b517f2e988febaec45c327743a773f0099410dee
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-08-24 01:59:48 +0300

    signal.c

---
+++ signal.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+/**
+ * signal_handler - handles the ctrl-c signal
+ * @signo: signal received
+ */
+
+void signal_handler(int signo)
+{
+	(void) signo;
+	_write("\n");
+	print_cmdline();
+}
